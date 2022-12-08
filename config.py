b10eee2643c80c987090c9bb61f07b187d3754a8
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2022-12-09 01:47:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+TIME_ZONE = os.environ["Asia/Kolkata"]
+BOT_LIST = [i.strip() for i in os.environ.get("@MrsNatashaBot, @AnonMusicBot, @NixaRobot").split(' ')]
+CHANNEL_OR_GROUP_ID = int(os.environ["-1001827333320"])
+MESSAGE_ID = int(os.environ["1"])
+BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("5616461719").split(' ')]
+GRP_ID = os.environ.get("-1001801801469")
+CHANNEL_NAME = os.environ.get("ʙᴏᴛs ᴜᴘᴅᴀᴛᴇ")
