8672a46aae5998c59bd3cb484ab5e0c226f89dd9
sukkunmusic <120463129+sukkunmusic@users.noreply.github.com>
2023-02-07 15:51:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ import os
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "26850449"))
-API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
-SESSION_STRING = getenv("SESSION_STRING", "AQCh_HmNiYrGMOVUfoQOXcHf57QJZ7LJ3eDuwJyHkK92SOHNGLUwbdLa0niP1eLFZepwA0oYItPWvba6myf2UDE1spKo5wulvuSCmpkFFIBvfaxBqxl7bFChXMh7e9T7y_w72xTP99BPqfbyt-TlEgobeRwqbuTGQahBsrwqchAnD87dO_WMxu2Fg9LT5G_Ekm2xwuRYvqSDJPUySdchKaeCxHa0Y8dnP7fTIu3xIBETsY7YcoODtEPiIuklZ6FemL2loZO2KaA3dL66tdlEkNOnr-WkEMCaRDAucjveFSd1DigQnJ-fSAQOh87XRERJoS04VJQSy-dkCIHD1mN-uDPuAAAAAVyIB7AA")
+API_ID = int(getenv("API_ID", "25181093"))
+API_HASH = getenv("API_HASH", "43ce5a99e3c109cbfc52291f9e51f6b2")
+SESSION_STRING = getenv("SESSION_STRING", "AQBhixA7xVK-M9ap93lXVPDtf5oEPPYVJPIE2a7btMbH4HyyDcCNsjW9uHSC_wrxrHE1LursktYugv2KVQHv7-sY1fOLhOUJA5bksACT8z16KjD-VGAUHjYUMBtx-5YtByRUTjYfJXcRvNoVZlNTOAWIjKQn0ukJnqzg2cuj3zDeZZ59B1_fEpDqchwYU0zV96VPT8a1lv-1zhCu0AASw8FQNMyD0WEd_VyTnXDCzwtWi04TEfiAXCdgHpqpq_WqYSE_rYIJLQIgGcLBV6wAGcnUJzMAQt2bd7y31fLOjbX8PvVoW5OEZ1wAY0t2sZFtQpNmpa8Jgio1ehSgRn4BPUesAAAAAUI_36sA")
 GROUP_ID = int(getenv("GROUP_ID", "-1001801801469"))
-CHANNEL_OR_GROUP_ID = int(getenv("CHANNEL_OR_GROUP_ID", "-1001827333320"))
-MESSAGE_ID = int(getenv("MESSAGE_ID", "30"))
-CHANNEL_NAME = getenv("CHANNEL_NAME", "ʙᴏᴛs ᴜᴘᴅᴀᴛᴇ")
+CHANNEL_OR_GROUP_ID = int(getenv("CHANNEL_OR_GROUP_ID", "-1001726732099"))
+MESSAGE_ID = int(getenv("MESSAGE_ID", "682"))
+CHANNEL_NAME = getenv("CHANNEL_NAME", "ᴜᴘᴅᴀᴛᴇ ᴜᴘᴅᴀᴛᴇ")
 TIME_ZONE = getenv("TIME_ZONE", "Asia/Kolkata")
-BOT_LIST = list(getenv("BOT_LIST", "MrsNatashaBot AnonMusicBot aina_musicbot").split())
-BOT_ADMIN_IDS = list(map(int, getenv("BOT_ADMIN_IDS", "5616461719").split()))
+BOT_LIST = list(getenv("BOT_LIST", "ainaxbot vickmusicrobot miffy_robot aina_musicbot").split())
+BOT_ADMIN_IDS = list(map(int, getenv("BOT_ADMIN_IDS", "5140509190 5803510096 5701306307").split()))
